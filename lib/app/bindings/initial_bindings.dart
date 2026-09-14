2fae423a0abc0d215d20ed8248d7810d886192db
Ivan Niku <62467537+Ivanniku22@users.noreply.github.com>
2026-09-14 17:05:06 +0530

    Location Service initialization

--- lib/app/bindings/initial_bindings.dart
+++ lib/app/bindings/initial_bindings.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 
 import '../../core/services/database_service.dart';
 import '../../core/services/firebase_service.dart';
+import '../../core/services/location_service.dart';
 import '../../data/repositories/auth_repository.dart';
 import '../../data/repositories/site_repository.dart';
 import '../../modules/auth/controller/auth_controller.dart';
@@ -16,6 +17,11 @@ class InitialBinding extends Bindings {
 
     Get.put<DatabaseService>(
       DatabaseService(),
+      permanent: true,
+    );
+
+    Get.put<LocationService>(
+      LocationService(),
       permanent: true,
     );
 

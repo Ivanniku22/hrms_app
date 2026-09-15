771343a66d9b1640d8d51bf643fa8d65aa501e87
Ivan Niku <62467537+Ivanniku22@users.noreply.github.com>
2026-09-15 20:45:46 +0530

    Bug fixes

--- lib/data/repositories/auth_repository.dart
+++ lib/data/repositories/auth_repository.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:get/get.dart';
 import '../../core/services/firebase_service.dart';
 import '../models/user_model.dart';
 
--- lib/modules/auth/bindings/auth_binding.dart
+++ lib/modules/auth/bindings/auth_binding.dart
@@ -1,7 +1,4 @@
 import 'package:get/get.dart';
-import '../../../core/services/firebase_service.dart';
-import '../../../data/repositories/auth_repository.dart';
-import '../controller/auth_controller.dart';
 
 class AuthBinding extends Bindings {
   @override
